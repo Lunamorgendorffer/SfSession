cf5d54b1649716167d1ec0b3156629814d78ce6a
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-12 12:36:39 +0200

    route app + show

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Session;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+     public function index(EntityManagerInterface  $entityManager): Response
     {
+        $sessions =  $entityManager->getRepository(Session::class)->findAll();
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions,
+        ]);
+    }
+
+
+    #[Route('/session/{id}', name: 'show_session')]
+    public function show(Session $session): Response
+    {
+        return $this->render('session/detailSession.html.twig', [
+           'session' => $session,
         ]);
     }
 }
