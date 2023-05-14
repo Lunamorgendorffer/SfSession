dfa0263bad4d37ecafa0cc64da90793ff11ffc4d
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-14 20:57:58 +0200

    first commit cat

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categorie', name: 'app_categorie')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $categories = $entityManager->getRepository(Categorie::class)->findBy([], ["intitule" => "ASC"]);
+        return $this->render('categorie/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/categorie/add', name: 'add_categorie')]
+    #[Route('/categorie/{id}/edit', name: 'edit_categorie')]
+    public function add(EntityManagerInterface $entityManager, Categorie $categorie = null, Request $request): Response 
+    {
+        if (!$categorie){ // si la categorie n'existe pas 
+            $categorie = new Categorie();  // alors crée un nouvel objet categorie 
+        }
+        // on crée le formulaire 
+        $form = $this->createForm(CategorieType::class, $categorie);
+        $form->handleRequest($request);
+
+        //quand on sousmet le formulaire 
+        if($form->isSubmitted() && $form->isValid()){
+
+            $categorie = $form->getData();
+            $entityManager->persist($categorie);// = prepare
+            $entityManager->flush();// execute, on envoie les données dans la db 
+
+            return $this->redirectToRoute('app_categorie');
+
+        }
+
+        // vue pour afficher le formulaire 
+        return $this->render('categorie/addcategorie.html.twig', [
+           'formAddcategorie' => $form->createView(),
+           'edit'=> $categorie->getId()
+            
+        ]);
+
+    }
+
+
+
+
+}
