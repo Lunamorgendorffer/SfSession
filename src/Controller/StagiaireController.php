9428d3535cebd7dd55a9a4c50c632c788ee9184b
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-14 18:10:26 +0200

    CRUD

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +20,48 @@ class StagiaireController extends AbstractController
                 'stagiaires' => $stagiaires,
             ]);
     }
+
+    #[Route('/stagiaire/add', name: 'add_stagiaire')]
+    #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+    public function add(EntityManagerInterface $entityManager, Stagiaire $stagiaire = null, Request $request): Response 
+    {
+        if (!$stagiaire){
+            $stagiaire = new stagiaire();
+        }
+        // on crée le formulaire 
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+        $form->handleRequest($request);
+
+        //quand on sousmet le formulaire 
+        if($form->isSubmitted() && $form->isValid()){
+
+            $stagiaire = $form->getData();
+            $entityManager->persist($stagiaire);// = prepare
+            $entityManager->flush();// execute, on envoie les données dans la db 
+
+            return $this->redirectToRoute('app_stagiaire');
+
+        }
+
+        // vue pour afficher le formulaire 
+        return $this->render('stagiaire/addStagiaire.html.twig', [
+           'formAddstagiaire' => $form->createView(),
+           'edit'=> $stagiaire->getId()
+            
+        ]);
+
+    }
+
+    #[Route('/stagiaire/{id}/delete', name: 'delete_stagiaire')]
+    public function delete(EntityManagerInterface $entityManager, Stagiaire $stagiaire): Response
+    {
+        $entityManager->remove($stagiaire);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_session');
+
+    }
+
     
     
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
